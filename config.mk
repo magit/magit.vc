eb4baabffc2c2bfe1762d03f72c69412506bac56
Jonas Bernoulli <jonas@bernoul.li>
2016-08-25 01:45:31 +0200

    remove manuals for with-editor releases that haven't happened yet

--- config.mk
+++ config.mk
@@ -1,5 +1,7 @@
-VERSION = 2.8
-VERSION_PREV = 2.7
+MAGIT_VERSION = 2.8
+MAGIT_VERSION_PREV = 2.7
+WITH_EDITOR_VERSION = 2.5
+WITH_EDITOR_VERSION_PREV = 2.4
 
 SITE_LISP  ?= ~/.emacs.d/lib
 MAGIT_REPO ?= $(SITE_LISP)/magit
@@ -14,4 +16,3 @@ BATCH  = $(EMACS) --batch -Q $(LOAD_PATH)
 TEXI2PDF ?= texi2pdf
 
 MANUAL_CSS = /css/page.css
-
